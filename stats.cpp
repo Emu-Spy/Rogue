ab12f9b2ba855a69c7b746993ce5b38e1f6e4e2e
SkylerHJ <49655631+SkylerHJ@users.noreply.github.com>
2019-04-15 13:08:53 -0500

    Add files via upload

---
+++ stats.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Stats
+{
+	private:
+		int values[6] = {0, 0, 0, 0, 0, 0};
+		int mods[6] = {};
+	
+	public:
+		void print();
+		void changeStat();
+};
