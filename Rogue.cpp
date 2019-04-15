8ac4ec126dbf8b017d992bce23ba4da0b4f4b570
Emu-Spy <49655257+Emu-Spy@users.noreply.github.com>
2019-04-15 13:16:28 -0500

    Update Rogue.cpp

--- Rogue.cpp
+++ Rogue.cpp
@@ -113,15 +113,4 @@ int Rogue :: four_dice_drop_low(){
 	stat=dice[1]+dice[2]+dice[3];
 	return stat;
 }
-//uses a formula to find modifiers for ability scores
-int Rogue :: find_modifier(int stat){
-	int mod;
-	//the modifier increases by 1 for every 2 over 10 and decreases by 1 for every 2 under 11. For 10 and 11 it is 0.
-	if(stat>11)
-		mod=(stat-11)/2;
-	else if(stat<10)
-		mod=((11-stat)/2)*-1;
-	else
-		mod=0;
-	return mod;	
-}
+
