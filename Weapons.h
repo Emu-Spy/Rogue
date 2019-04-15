c9d3bc2398cb584debe5148e5456248bfdfc3cb2
jellui <49655633+jellui@users.noreply.github.com>
2019-04-15 13:27:07 -0500

    Add files via upload
    struct for weapons

---
+++ Weapons.h
@@ -0,0 +1,15 @@
+//Names: Ashanti, Skyler, Erika, Logan, Joshua
+
+struct Weapons
+{
+	bool martialRange;
+	bool martialMelee;
+	bool simpleRange;
+	bool simpleMelee;
+	int cost;
+	int weight;
+	string damage;
+	
+	
+	
+};
