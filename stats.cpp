8a36f7de82bfd991315ebdeee5fb6a82d1e3275c
Emu-Spy <49655257+Emu-Spy@users.noreply.github.com>
2019-04-15 13:15:48 -0500

    Create stats.cpp

---
+++ stats.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "stats.h"
+using namespace std;
+
+//uses a formula to find modifiers for ability scores
+int stats :: find_modifier(int stat){
+	int mod;
+	//the modifier increases by 1 for every 2 over 10 and decreases by 1 for every 2 under 11. For 10 and 11 it is 0.
+	if(stat>11)
+		mod=(stat-11)/2;
+	else if(stat<10)
+		mod=((11-stat)/2)*-1;
+	else
+		mod=0;
+	return mod;	
+}
