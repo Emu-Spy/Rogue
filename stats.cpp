cbc41b287f0a5f51fd55b2c06e7f519a02abf11f
Emu-Spy <49655257+Emu-Spy@users.noreply.github.com>
2019-04-15 13:16:22 -0500

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //uses a formula to find modifiers for ability scores
-int stats :: find_modifier(int stat){
+int stats :: findModifier(int stat){
 	int mod;
 	//the modifier increases by 1 for every 2 over 10 and decreases by 1 for every 2 under 11. For 10 and 11 it is 0.
 	if(stat>11)
