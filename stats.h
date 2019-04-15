1289d2b5ac75abd77a6c76d218d20d0d1f0807c9
NotVirtualism <49655298+NotVirtualism@users.noreply.github.com>
2019-04-15 13:21:10 -0500

    Update stats.h

--- stats.h
+++ stats.h
@@ -4,10 +4,12 @@ using namespace std;
 class Stats
 {
 	private:
-		int values[6] = {0, 0, 0, 0, 0, 0};
-		int mods[6] = {};
+		int values[6];
+		int mods[6];
 	
 	public:
+		Stats();
+		Stats(int p_values[]);
 		void print();
 		void changeStat();
 		void findModifier();
