fcdaf570a8c390bd86f89eae5861d30e070a3521
NotVirtualism <49655298+NotVirtualism@users.noreply.github.com>
2019-04-17 13:58:40 -0500

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -57,8 +57,21 @@ int Stats :: four_dice_drop_low(){
 
 void Stats :: print ()
 {
-	for (int i = 0; i < 6; i++)
-	{
-		cout<< values[i];
-	}
+	cout << "Strength: " << values[0] << ", Mod: " << mods[0] << endl;
+	cout << "Dexterity: " << values[1] << ", Mod: " << mods[1] << endl;
+	cout << "Constitution: " << values[2] << ", Mod: " << mods[2] << endl;
+	cout << "Intelligence: " << values[3] << ", Mod: " << mods[3] << endl;
+	cout << "Wisdom: " << values[4] << ", Mod: " << mods[4] << endl;
+	cout << "Charisma: " << values[5] << ", Mod: " << mods[5] << endl;
+}
+
+void Stats :: changeStat(int stat, int num)
+{
+	values[stat] = num;
+	if(num>11)
+		mods[stat]=(num-11)/2;
+	else if(num<10)
+		mods[stat]=((11-num)/2)*-1;
+	else
+		mods[stat]=0;
 }
