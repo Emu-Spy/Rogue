c96a37280cc66747b2f0a4a2a24a5b5a3a4f4733
SkylerHJ <49655631+SkylerHJ@users.noreply.github.com>
2019-04-23 13:09:54 -0500

    Add files via upload

---
+++ RogueRunner.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<< " Score       Cost\n" << "-------     -------" << endl;
+	cout<< "8           0\n" << "9           1\n" << "10          2\n" << "11          3\n";
+	cout<< "12          4\n" << "13          5\n" << "14          7\n" << "15          9\n" << endl;
+	//print out default stats (8 in all stats)
+	cout<< "You "
+	cout<< "How many points would you like in each of the following stats:" << endl;
+	
+	
+	return 0;
+}
