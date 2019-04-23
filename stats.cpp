343f55cba9a98ac34560de23385f6cf072cc550e
SkylerHJ <49655631+SkylerHJ@users.noreply.github.com>
2019-04-23 13:54:26 -0500

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -13,20 +13,6 @@ Stats :: Stats(){
 int Stats :: findModifier(int stat){
 	return mods[stat-1];
 }
-//rolls up a set of stats
-void Stats :: roll_stats(){
-	//calls the fourdicedroplowest method 6 times, representing rolling 6 stats.
-	for (int i = 0; i < 6; i++)
-	{
-		values[i]=four_dice_drop_low();
-		if(values[i]>11)
-			mods[i]=(values[i]-11)/2;
-		else if(values[i]<10)
-			mods[i]=((11-values[i])/2)*-1;
-		else
-			mods[i]=0;
-	}
-}
 //this method rolls four six-sided dice and ignores the lowest one. Then it adds them all together to get a stat from 3-18
 int Stats :: four_dice_drop_low(){
 	int stat;
