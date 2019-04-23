f936b6def9f7be4c758ca2332be1058d112ae80c
SkylerHJ <49655631+SkylerHJ@users.noreply.github.com>
2019-04-23 13:24:38 -0500

    Update RogueRunner.cpp

--- RogueRunner.cpp
+++ RogueRunner.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+	
+	cout<< "Welcome to your guide through making a Rogue class DnD cahracter.";
+	cout<< "Here you will be able to set your characters ability scores, weaponry, age, and race." << endl;
+	
+	
+	
 	cout<< " Score       Cost\n" << "-------     -------" << endl;
 	cout<< "8           0\n" << "9           1\n" << "10          2\n" << "11          3\n";
 	cout<< "12          4\n" << "13          5\n" << "14          7\n" << "15          9\n" << endl;
@@ -15,17 +21,19 @@ int main()
 	cout<< "How many points would you like in each of the following stats:" << endl;
 	
 	cout<< "Strength: ";
-	cin<< //var;
+	cin>> //var;
 	cout<< "\nDexterity: ";
-	cin<< //var;
+	cin>> //var;
 	cout<< "\nConstitution: ";
-	cin<< //var;
+	cin>> //var;
 	cout<< "\nIntellignece: ";
-	cin<< //var;
+	cin>> //var;
 	cout<< "\nWisdom: ";
-	cin<< //var;
+	cin>> //var;
 	cout<< "\nCharisma: ";
-	cin<< //var;
+	cin>> //var;
+	
+	
 	
 	
 	return 0;
