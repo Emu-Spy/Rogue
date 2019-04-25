6e6133cc3743f5b0f162d82dc9c63f8851a4e605
Emu-Spy <49655257+Emu-Spy@users.noreply.github.com>
2019-04-25 12:57:02 -0500

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -18,7 +18,7 @@ Stats :: Stats(){
 int Stats :: calcModifier(int stat){
 	int mod;
 	//the modifier increases by 1 for every 2 over 10 and decreases by 1 for every 2 under 11. For 10 and 11 it is 0.
-	if(stat>11)
+	if(stat>10)
 		mod=(stat-10)/2;
 	else if(stat<10)
 		mod=((10-stat)/2)*-1;
