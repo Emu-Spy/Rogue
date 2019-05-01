2686a40220cb3f7e7fa53d12a82b5deb51438e4b
Emu-Spy <49655257+Emu-Spy@users.noreply.github.com>
2019-05-01 12:54:56 -0500

    Update Race.h

--- Race.h
+++ Race.h
@@ -7,8 +7,6 @@ class Race
 {
 	private:
 		string race;
-		int raceBonus[6];
-		bool skillProficiencies[18];
 		bool darkvision;
 		int speed;
 		vector <string> languages;
@@ -17,7 +15,4 @@ class Race
 		Race();
 		void changeRace(string p_race);
 		void racePrint();
-		//TODO: code these methods and then use them in rogue.cpp
-		int* returnBonuses();
-		int* returnProficiencies();
 };
